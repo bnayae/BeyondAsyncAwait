40cb6610ec82e1c564b5b96e45e90e7edef44c0a
bnaya eshet <bnayanet@hotmail.com>
2017-05-09 08:33:11 +0300

    better service client

--- CommonServiceMistakesClient/Program.cs
+++ CommonServiceMistakesClient/Program.cs
@@ -9,33 +9,40 @@ namespace CommonServiceMistakesClient
     class Program
     {
         private const int MAX = 50;
-        private const int PORT = 56368; // Core IIS Express
-        //private const int PORT = 56369; // Core Exe
-        //private const int PORT = 32769; // Core Docker
-        //private const int PORT = 50257; // FX
+
+        private static readonly (string Title, string Server)[] SERVERS =
+        {
+            ("Core on IIS Express", "localhost:56368"),
+            ("Core on exe", "localhost:56369"),
+            ("Core on Docker", "localhost:32769"),
+            ("Fx on IIS Express", "localhost:50257"),
+        };
+
         private static readonly (string Title, string Url)[] END_POINTS =
         {
-            ("Right", $"http://localhost:{PORT}/api/sample/right/"),
-            ("Needless", $"http://localhost:{PORT}/api/sample/needless/"),
-            ("Stupid", $"http://localhost:{PORT}/api/sample/stupid/"),
-            ("Silly", $"http://localhost:{PORT}/api/sample/silly/ "),
-            ("Sync", $"http://localhost:{PORT}/api/sample/sync/"),
+            ("Right", "http://{0}/api/sample/right/"),
+            ("Needless", "http://{0}/api/sample/needless/"),
+            ("Stupid", "http://{0}/api/sample/stupid/"),
+            ("Silly", "http://{0}/api/sample/silly/ "),
+            ("Sync", "http://{0}/api/sample/sync/"),
         };
+
+        private static string _server;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Warm-up");
+            ServerSelectionAndWarmup();
 
-            Warmup();
             for (int i = 0; i < 10; i++)
             {
-                foreach (var endPoint in END_POINTS)
+                foreach (var endPointTemplate in END_POINTS)
                 {
+                    string endPoint = string.Format(endPointTemplate.Url, _server);
                     Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine($"==================  Start {endPoint.Title} ==================");
+                    Console.WriteLine($"==================  Start {endPointTemplate.Title} ==================");
                     Console.ResetColor();
 
-                    Task test = CallAsync(endPoint.Url, MAX);
+                    Task test = CallAsync(endPoint, MAX);
                     test.Wait();
                     Console.WriteLine("Press any key for the next test...");
                     Console.ReadKey(true);
@@ -43,6 +50,31 @@ namespace CommonServiceMistakesClient
             }
 
             Console.ReadKey();
+        }
+
+        private static void ServerSelectionAndWarmup()
+        {
+            Console.WriteLine("Select Server");
+            for (int i = 0; i < SERVERS.Length; i++)
+            {
+                string server = SERVERS[i].Title;
+                Console.WriteLine($"    {i}. {server}");
+            }
+            char c = Console.ReadKey(true).KeyChar;
+            int selection = int.Parse(c.ToString());
+            _server = SERVERS[selection].Server;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Targeting: {SERVERS[selection].Title}");
+            Console.ResetColor();
+
+            Console.WriteLine("Warm-up");
+            Warmup();
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Targeting: {SERVERS[selection].Title}");
+            Console.ResetColor();
         }
 
         private static async Task CallAsync(string root, int times)
@@ -85,7 +117,9 @@ namespace CommonServiceMistakesClient
             {
                 try
                 {
-                    var warmups = END_POINTS.Select(endPoint => CallAsync(endPoint.Url, 1));
+                    var warmups = END_POINTS
+                        .Select(m => string.Format(m.Url, _server))
+                        .Select(endPoint => CallAsync(endPoint, 1));
                     Task.WaitAll(warmups.ToArray());
                     break;
                 }
