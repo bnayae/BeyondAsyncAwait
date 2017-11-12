64c8bad1f4080cc57bacf14d5be387ca81ccdda7
bnaya eshet <bnayanet@hotmail.com>
2017-11-12 23:15:34 +0200

    TDF +

---
+++ 02.1 Async Download/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _02._1_Async_Download
+{
+    class Program
+    {
+        private const string URL = "http://lorempixel.com/1200/1200/animals/";
+
+        static void Main(string[] args)
+        {
+            Task t = ProcessAsync();
+           
+            while (!t.IsCompleted)
+            {
+                Console.Write(".");
+                Thread.Sleep(10);
+            }
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        private static async Task ProcessAsync()
+        {
+            Task<byte[]> t1 = DownloadAsync(URL);
+            Task<byte[]> t2 = DownloadAsync(URL);
+            byte[][] images = await Task.WhenAll(t1, t2);
+            int i = 0;
+            foreach (byte[] data in images)
+            {
+                await SaveAsync($"Image {i++}.jpg", data);
+            }           
+        }
+        private static async Task<byte[]> DownloadAsync (string url)
+        {
+            using (var http = new HttpClient())
+            {
+                byte[] data = await http.GetByteArrayAsync(url);
+                return data;
+            }
+        }
+        private static async Task SaveAsync (string name, byte[] data)
+        {
+            using (var fs = new FileStream(name, FileMode.Create, FileAccess.Write, FileShare.None, 4096, FileOptions.Asynchronous))
+            {
+                await fs.WriteAsync(data, 0, data.Length);
+            }
+        }
+    }
+}
---
+++ 20. Useful Extensions Core Client/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _20._Useful_Extensions_Core_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task _ = CheckDeadlockAsync();
+            _ = CheckTimeoutAsync();
+            _ = MultiExceptionAsync();
+            _ = ExecAsync();
+
+            var cts = new CancellationTokenSource();
+            cts.Token.RegisterWeak(() => throw new InvalidCastException("Shit may happen"));
+            if(!cts.CancelSafe())
+                Console.WriteLine("shit does happen");
+
+            Console.ReadKey();
+        }
+
+        private static async Task CheckDeadlockAsync()
+        {
+            try
+            {
+                Task t = Task.Delay(1000);
+                // check for potential deadlock
+                if (await t.IsTimeoutAsync(TimeSpan.FromMilliseconds(100)))
+                    Console.WriteLine("Potential deadlock");
+                await t;
+                Console.WriteLine("Eventually it pass");
+            }
+            catch (TimeoutException)
+            {
+                Console.WriteLine("Took longer than expected");
+            }
+        }
+
+        private static async Task CheckTimeoutAsync()
+        {
+            try
+            {
+                await Task.Delay(1000).WithTimeout(TimeSpan.FromMilliseconds(100));
+                throw new Exception("Expecting timeout");
+            }
+            catch (TimeoutException)
+            {
+                Console.WriteLine("Took longer than expected");
+            }
+        }
+
+        private static async Task MultiExceptionAsync()
+        {
+            Task a = Task.Run(() => throw new IndexOutOfRangeException("Error A"));
+            Task b = Task.Run(() => throw new IndexOutOfRangeException("Error B"));
+
+            try
+            {
+                await Task.WhenAll(a, b).ThrowAll();
+            }
+            catch (AggregateException exs)
+            {
+                var flatten = exs.Flatten();
+                Console.WriteLine($"Catch {flatten.InnerExceptions.Count} exceptions");
+                foreach (var ex in flatten.InnerExceptions)
+                {
+                    Console.WriteLine(ex.GetBaseException().Message);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Catch single exception");
+            }
+        }
+
+        private static async Task ExecAsync()
+        {
+            try
+            {
+                await Exec1Async();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Format());
+            }
+
+        }
+        private static async Task Exec1Async()
+        {
+            await Task.Delay(1);
+            await Exec2Async();
+        }
+        private static async Task Exec2Async()
+        {
+            await Task.Delay(1);
+            await Exec3Async();
+        }
+        private static async Task Exec3Async()
+        {
+            await Task.Delay(1);
+            throw new InvalidOperationException("Some error");
+        }
+    }
+}
