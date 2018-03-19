aad2dbdd6a6e35b5f381644f5f4eadc37724fce9
bnaya eshet <bnayanet@hotmail.com>
2018-03-19 15:27:59 +0200

    batch and awaitable (draft)

---
+++ BatchSync/Program.cs
@@ -0,0 +1,123 @@
+﻿using SkiaSharp;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace BatchSync
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var options = new GroupingDataflowBlockOptions { Greedy = false };
+            var sync = new BatchBlock<(int, double)>(3, options);
+
+            // the delegate returns value tuple (.MET 4.7 / standard 2.0)
+            var chA = new TransformBlock<int, (int, double)>(
+                (Func<int, (int, double)>)ChannelA);
+            var chB = new TransformBlock<int, (int, double)>(
+                (Func<int, (int, double)>)ChannelB);
+            var chC = new TransformBlock<int, (int, double)>(
+                (Func<int, Task<(int, double)>>)ChannelC);
+
+            var presenter = new ActionBlock<(int, double)[]>((Action<(int, double)[]>)Present);
+
+            chA.LinkTo(sync);
+            chB.LinkTo(sync);
+            chC.LinkTo(sync);
+            sync.LinkTo(presenter);
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                chA.Post(i);
+                chB.Post(i);
+                chC.Post(i);
+            }
+
+            Console.ReadKey();
+        }
+
+        #region ChannelA
+
+        private static (int, double) ChannelA(int input) => (input, input * 1.2);
+
+        #endregion // ChannelA
+
+        #region ChannelB
+
+        private static (int, double) ChannelB(int input)
+        {
+            int calc = input;
+            for (int i = 0; i < 10000; i++)
+            {
+                if ((calc * calc) % 2 == 0)
+                    calc += calc % 10;
+                else
+                    calc -= calc % 10;
+            }
+            return (input,calc);
+        }
+
+        #endregion // ChannelB
+
+        #region ChannelC
+
+        private static async Task<(int, double)> ChannelC(int input)
+        {
+            int calc = input;
+            for (int i = 0; i < 10; i++)
+            {
+                int x = Environment.TickCount % 10 - 5;
+                calc += x;
+                await Task.Delay(100).ConfigureAwait(false);
+            }
+            return (input, calc); /* value tuple */
+        }
+
+        #endregion // ChannelC
+
+        #region Present
+
+        public static void Present((int correlate, double calc)[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.SetCursorPosition(i * 10, Console.CursorTop);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(data[i].correlate);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($": {data[i].calc}");
+            }
+            Console.WriteLine();
+        }
+
+        #endregion // Present
+
+        #region Message (nested)
+
+        private class Message
+        {
+            public Message(int correlate, int input)
+            {
+                Input = input;
+                Correlate = correlate;
+            }
+
+            public int Input { get; }
+            public int Correlate { get; }
+        }
+
+        #endregion // Message (nested)
+    }
+
+    public class EndMessage
+    {
+        public double Origin { get; set; }
+        public double ChannelA { get; set; }
+        public double ChannelB { get; set; }
+        public double ChannelC { get; set; }
+    }
+}
---
+++ ValueTaskBenchmark46/AsyncBenchmark.cs
@@ -0,0 +1,73 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Engines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bnaya.Samples
+{
+    [ClrJob]
+    //[CoreJob]
+    //[MonoJob]
+    [RankColumn, MinColumn, MaxColumn]
+    //[SimpleJob(RunStrategy.Throughput, launchCount: 10, warmupCount: 0, targetCount: 100)]
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+    public class AsyncBenchmark
+    {
+        //private const int OPERATIONS_PER_INVOKE = 1000;
+
+        [BenchmarkCategory("FromResult")]
+        [Benchmark(Description = "FromResult")] //, OperationsPerInvoke = OPERATIONS_PER_INVOKE)]
+        public Task<int> TaskFromResult() => Task.FromResult(1);
+
+        [BenchmarkCategory("FromResult")]
+        [Benchmark(Baseline = true, Description = "new ValueTask")] //, OperationsPerInvoke = OPERATIONS_PER_INVOKE)]
+        public ValueTask<int> ValueTaskFromResult() => new ValueTask<int>(1);
+
+        [BenchmarkCategory("Async+FromResult")]
+        [Benchmark(Description = "await Task")] //, OperationsPerInvoke = OPERATIONS_PER_INVOKE)]
+        public async Task<int> TaskAsyncFromResult()
+        {
+            await Task.FromResult(1);
+            await Task.Run(() => { });
+            await Task.FromResult(1);
+            return 1;
+        }
+
+        [BenchmarkCategory("Async+FromResult")]
+        [Benchmark(Description = "await ValueTask")] //, OperationsPerInvoke = OPERATIONS_PER_INVOKE)]
+        public async ValueTask<int> ValueTaskAsyncFromResult()
+        {
+            await new ValueTask<int>(1);
+            await Task.Run(() => { });
+            await new ValueTask<int>(1);
+            return 1;
+        }
+
+
+        [BenchmarkCategory("Async+FromResult+ConfigureAwait")]
+        [Benchmark(Description = "await Task.Configure")] //, OperationsPerInvoke = OPERATIONS_PER_INVOKE)]
+        public async Task<int> TaskAsyncFromResultConfigureAwait()
+        {
+            await Task.FromResult(1).ConfigureAwait(false);
+            await Task.Run(() => { }).ConfigureAwait(false);
+            await Task.FromResult(1).ConfigureAwait(false);
+            return 1;
+        }
+
+        [BenchmarkCategory("Async+FromResult+ConfigureAwait")]
+        [Benchmark(Description = "await ValueTask.Configure")] //, OperationsPerInvoke = OPERATIONS_PER_INVOKE)]
+        public async ValueTask<int> ValueTaskAsyncFromResultConfigureAwait()
+        {
+            await new ValueTask<int>(1).ConfigureAwait(false);
+            await Task.Run(() => { }).ConfigureAwait(false);
+            await new ValueTask<int>(1).ConfigureAwait(false);
+            return 1;
+        }
+    }
+}
